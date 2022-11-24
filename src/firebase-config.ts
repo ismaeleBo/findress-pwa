05b0034d7f5256244ef39505f00450353f6c233b
ismaeleBo <ismaelebonaventura@gmail.com>
2022-11-24 18:49:51 +0100

    chore: migrate firebase server

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -6,13 +6,13 @@ import { getStorage } from 'firebase/storage';
 // import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCMVvB2hN79lz-4MoLSbCaPZfcnMoDZo6Q',
-  authDomain: 'findress-e577d.firebaseapp.com',
-  projectId: 'findress-e577d',
-  storageBucket: 'findress-e577d.appspot.com',
-  messagingSenderId: '540603963345',
-  appId: '1:540603963345:web:c63b629b621f8d0eba72f0',
-  measurementId: 'G-XK1YJ1ZMHQ',
+  apiKey: 'AIzaSyC9DYK8WOFpBXX7fofV__jC5hOebsSDsUQ',
+  authDomain: 'findress-9b6b9.firebaseapp.com',
+  projectId: 'findress-9b6b9',
+  storageBucket: 'findress-9b6b9.appspot.com',
+  messagingSenderId: '819953521954',
+  appId: '1:819953521954:web:981c3f241ee1fd96ee017b',
+  measurementId: 'G-KEBFXX2JB1',
 };
 
 const app = initializeApp(firebaseConfig);
